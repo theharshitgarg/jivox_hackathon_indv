d36b08381448d317a00dcf78694b22c1d941a770
harshit garg <harshit@esthenos.com>
2016-08-06 11:36:07 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#AUTHOR : HARSHIT GARG
+#EMAIL : harshitgargmnit@gmail.com
+#DATE : AUGUST 6, 2016
+#MAIN FILE
+
+#STATUS : INCOMPLETE
+
+
+from sorting import *
+
+
+def main():
+	print "MAIN:"
+	
+
+if '__main__'==__name__:
+	main()
+
---
+++ sorting.py
@@ -0,0 +1,72 @@
+#AUTHOR : HARSHIT GARG
+#DATE : AUGUST 6, 2016
+#SORTING FUNCTIONS LIST
+
+
+debug = True
+
+def bubble_sort(input_list):
+	if debug :
+		print "*"*28
+		print "BB Sort"
+		print "*"*28
+		
+		
+
+def insertion_sort(input_list):
+	'''
+		This is bubble sort
+	'''
+	if debug :
+		print "*"*28
+		print "BB Sort"
+		print "*"*28
+	input_length = len(input_list)
+	for i in xrange(input_length):
+		for j in xrange():
+			pass
+				
+
+
+
+def selection_sort(input_list):
+	'''
+		This is selection sort
+	'''
+	if debug :
+		print "*"*28
+		print "BB Sort"
+		print "*"*28
+
+
+def quick_sort(input_list):
+	'''
+		This is quick sort
+	'''
+	if debug :
+		print "*"*28
+		print "BB Sort"
+		print "*"*28
+		
+
+def merge_sort(input_list):
+	'''
+		This is merge sort
+	'''
+	if debug :
+		print "*"*28
+		print "BB Sort"
+		print "*"*28		
+				
+
+def heap_sort(input_list):
+	'''
+		This is head sort
+	'''
+	if debug :
+		print "*"*28
+		print "HEAP Sort"
+		print "*"*28		
+
+
+
